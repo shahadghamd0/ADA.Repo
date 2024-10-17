793072a039600d0c2d87911534f3a6347d5584b1
shahad <shahad.ghamd@gmail.com>
2024-10-17 20:45:11 +0300

    Initial Commit

---
+++ Learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning
+//
+//  Created by Shahad on 14/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningTests/LearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningTests.swift
+//  LearningTests
+//
+//  Created by Shahad on 14/04/1446 AH.
+//
+
+import Testing
+@testable import Learning
+
+struct LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
